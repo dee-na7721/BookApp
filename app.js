b46a601a39148c5ad8c0905af2dee7e674ab97b2
dee-na7721 <deenasitikhu@gmail.com>
2020-11-03 14:06:44 +0545

    add-delete-edit with mongodb

--- app.js
+++ app.js
@@ -4,12 +4,18 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser= require('body-parser');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var booksRouter = require('./routes/books');
+const { Mongoose } = require('mongoose');
 
 var app = express();
+
+mongoose.connect('mongodb://localhost/BookApp', {
+  useUnifiedTopology: true, useNewUrlParser: true},
+  ()=>{console.log('connected to db')});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var router = express.Router();
 var books = require('../resources/books')
+let Books = require('../models/books');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'BookApp',bookList:books }, );
-});
+router.get('/', async function(req, res, next) {
+  // Books.find({}, function(err, books) {
+    let books= await Books.find();
+    // if (!err) {
+      res.render('index', { title: 'Book App', bookList: books });
+    // } else {
+      // console.log('error', err);
+    // }
+  })
+
+  
+  // res.render('index', { title: 'BookApp',bookList:books }, );
+// });
 
 
 router.get('/deena', function(req, res, next) {
